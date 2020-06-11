ac482c4fef93f9fe174bfe0c9a8b4301096ce8bf
Yi Ou <ouyi@gmx.de>
2020-06-11 21:21:22 +0200

    Fix the principal name issue

--- src/main/java/io/github/ouyi/keycloakdemo/controller/LibraryController.java
+++ src/main/java/io/github/ouyi/keycloakdemo/controller/LibraryController.java
@@ -2,17 +2,12 @@ package io.github.ouyi.keycloakdemo.controller;
 
 
 import io.github.ouyi.keycloakdemo.repository.BookRepository;
-import org.keycloak.KeycloakSecurityContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Enumeration;
 
 @Controller
 public class LibraryController {
@@ -49,8 +44,7 @@ public class LibraryController {
     }
 
     private void configCommonAttributes(Model model, HttpServletRequest request) {
-        KeycloakSecurityContext keycloakSecurityContext = (KeycloakSecurityContext)(request.getAttribute(KeycloakSecurityContext.class.getName()));
-        model.addAttribute("name", keycloakSecurityContext.getIdToken().getGivenName());
+        model.addAttribute("name", request.getUserPrincipal().getName());
     }
 
 }

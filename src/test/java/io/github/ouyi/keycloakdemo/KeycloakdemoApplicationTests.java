6e545bc4b25df737f21b4b4ce586b3fa8eeb4eb2
Yi Ou <ouyi@gmx.de>
2020-06-11 14:41:46 +0200

    Project set up with spring initializer

---
+++ src/test/java/io/github/ouyi/keycloakdemo/KeycloakdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ouyi.keycloakdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
